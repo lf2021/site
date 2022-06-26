8cce7e122a404f34c45bb9c9211e81ee2e392929
Lee <lifan.van@bytedance.com>
2022-06-26 20:55:16 +0800

    chore: add awesome text when running

--- scripts/echo.js
+++ scripts/echo.js
@@ -1,1 +1,16 @@
-console.log('start running...')
+// 可以使用 vscode 插件 ASCIIDecorator 来生成这种文字
+// use: https://marketplace.visualstudio.com/items?itemName=helixquar.asciidecorator
+
+console.log(`
+                                                                                               
+                                88                                                           
+                                88                                                           
+                                88                                                           
+8b      db      d8   ,adPPYba,  88   ,adPPYba,   ,adPPYba,   88,dPYba,,adPYba,    ,adPPYba,  
+'8b    d88b    d8'  a8P_____88  88  a8"     ""  a8"     "8a  88P'   "88"    "8a  a8P_____88  
+ '8b  d8''8b  d8'   8PP"""""""  88  8b          8b       d8  88      88      88  8PP"""""""  
+  '8bd8'  '8bd8'    "8b,   ,aa  88  "8a,   ,aa  "8a,   ,a8"  88      88      88  "8b,   ,aa  
+    YP      YP       '"Ybbd8"'  88   '"Ybbd8"'   '"YbbdP"'   88      88      88   '"Ybbd8"'  
+                                                                                             
+                                                                                             
+`)
