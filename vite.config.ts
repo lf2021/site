3f125942631e269592cf1dc57c33cf0364c559ed
Lee <lifan.van@bytedance.com>
2022-06-26 20:30:18 +0800

    fix: alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const path = require('path');
 export default defineConfig({
   plugins: [react()],
   build: {
+    assetsDir: 'site/assets',
     rollupOptions: {
       output: {
         manualChunks: {
@@ -18,7 +19,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      'src': path.resolve(__dirname, 'src'),
     },
   },
   css: {
